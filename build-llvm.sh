b89b6dfb1f732105af5883b75863933295d9c7af
pranav083 <kpranav083@gmail.com>
2025-02-20 18:59:38 -0500

    llvm_19.1.7 with http
    Signed-off-by: pranav083 <kpranav083@gmail.com>

--- build-llvm.sh
+++ build-llvm.sh
@@ -4,10 +4,10 @@
 # LLVM version: 15.0.0 older version at checkout only
 # latest LLVM 19.1.7  build at tag 
 ROOT=$(pwd)
-git clone -b llvmorg-19.1.7 --single-branch git@github.com:llvm/llvm-project.git
+git clone -b llvmorg-19.1.7 --single-branch https://github.com/llvm/llvm-project.git
 cd $ROOT/llvm-project
 #git checkout e758b77161a7
-git checkout llvmorg-19.1.7
+#git checkout llvmorg-19.1.7
 if [ ! -d "build" ]; then
   mkdir build
 fi
